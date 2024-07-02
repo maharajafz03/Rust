389718723cce23773b821360962605c11fc7af4d
maharajafz03 <maharajafz1999@gmail.com>
2024-07-02 20:04:33 +0530

    [--learning_rust--]

--- src/main.rs
+++ src/main.rs
@@ -27,9 +27,9 @@
 // }
 
 
-// let mut greeting: &str = "hello world";
+// let mut greeting: String = String::from("hello world");
 
-//  greeting.push(str: "mother fucker");
+//  greeting.push_str(String "mother fucker");
 
 //   println!("{}", greeting);
 
@@ -40,6 +40,7 @@
 //     println!("Signed Integer: {}", x);
 //     println!("Unsigned Integer: {}", y);
 // }
+
 
 fn main(){
     // char in rust::::::
@@ -61,4 +62,51 @@ fn main(){
 
     let human: (&str, i32, bool, [i32; 6]) = ("allen", 30, true, [11,2,3,4,5,908]);
     println!("{:?}", human);
+
+    //slice::::::
+
+    let num:&[i32] = &[1,2,3,4,5];
+    println!("{:?}", num);
+
+    // ---------------->
+    
+
+    let mut  my_name = String::from("hello");
+    
+     my_name.push_str("mother Fucker");
+     println!("{}", my_name);
+
+     //String-------|stack & Heap|-------String
+
+     let mut data: String = String::from("welcome da pundai");
+     data.push_str("hows that fucks");
+     println!("{}", data);
+
+    //vec<T>-------------method
+
+    //  let mut num: Vec<i32> = Vec::new(21);
+    //  num.push(4);
+    //  num.push(7);
+    //  num.push(5);
+    //  num.push(2);
+    //  println!("{:?}", num);
+
+    // Create a mutable vector of integers
+    let mut numbers: Vec<i32> = Vec::new();
+
+    // Push integer values into the vector
+    numbers.push(10);
+    numbers.push(20);
+    numbers.push(30);
+
+    // Print the vector
+    println!("{:?}", numbers); // Output: [10, 20, 30]
+
+    let mut greeting: String = String::from("hello world");
+    println!("{}", greeting);
+    greeting.push_str( " mother fucker i love rust");
+    println!("{}", greeting);
+    let naan:String = greeting;
+    println!("{}", naan);
+
 }
