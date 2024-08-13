8b626e029137cbaf20f7ddf29c5fa64be20c9af8
maharajafz03 <maharajafz1999@gmail.com>
2024-08-13 15:34:23 +0530

    {adding}

--- src/data/datatypes.rs
+++ src/data/datatypes.rs
@@ -15,8 +15,37 @@ fn main() {
 
     //let name: String = f"hello";
 
+    let pi: f32 = 3.45;
+   // float value
+
     println!("{}",number);
     println!("{}",num1);
     println!("{}",unsigned);
-    
+    println!("{}", pi);
+
+    let male: bool = true || false;
+    println!("{}", male);
+
+    let array: [i32; 4] = [1,2,3,4];
+     // array should be look like this 
+    println!("{:?}", array);
+
+    let bikes: [&str; 2] = ["s1000rr", "ducati"];
+    println!("{:?}", bikes);
+    println!("{}", bikes[1]);
+
+    let human   = ("maga".to_string(), 20, true);
+    println!("{:?}", human);
+
+    //slices
+
+    let  maga :&[&str] = &["raja", "yamu", "nna"];
+    println!("{:?}", maga);
+
+    let manage: &[i32] = &[1,3,4,5];
+    println!("{:?}", manage);
+
+    let animal: &[&String] = &[&"maga".to_string() , &"raja".to_string()];
+    println!("{:?}", animal)
+
 }
