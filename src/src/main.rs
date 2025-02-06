9528dded43eaf0c263d8c954b0558da95f252d5d
maharajafz03 <maharajafz1999@gmail.com>
2025-02-06 14:58:36 +0530

    [avae..]

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,11 +1,24 @@
 fn main() {
- println!("{}", is_number(2));
+  println!("{}", fib(3));
 }
-fn is_number(num: i32) -> bool {
-    
-    if num % 2 == 0 {
-        return true;
-    }else {
-        return false;
+
+fn fib(num: i32) -> i32 {
+    let mut first = 0;
+    let mut second = 1;
+
+    if num == 0 {
+        return first;
     }
+
+    if num ==  1 {
+        return second;
+    }
+
+    for _ in 0..(num -1) {
+        
+        let temp = second;
+        second = second + first;
+        first = temp;
+    }
+    return second;
 }
