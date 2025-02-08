38828f90f330001bf6a1ad01cd5b1e2af525d65a
maharajafz03 <maharajafz1999@gmail.com>
2025-02-08 19:57:49 +0530

    [ashle]

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,15 +1,27 @@
+struct Car{
+    brand: String,
+    model: String
+}
 
-struct User{
-    name: String,
-    age: u8,
+impl Car{
 
+    fn display(&self) {
+        println!("{} {}", self.brand, self.model);
+    }
+
+    fn update(&mut self, brand: String){
+        self.brand = brand;
+    }
 }
 
 fn main() {
-   let user = User{
-    name: String::from("mother_fuckerz"),
-    age: 28,
-   };
 
-   println!("{}", user.name);
+    let mut car = Car{
+        brand: String::from("tesla"),
+        model: String::from("model_3")
+    };
+
+    car.display();
+    car.update(String::from("lamborghini"));
+    car.display();
 }
