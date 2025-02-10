d6dc4ace42e1c1e7717e378b8680ac41dfc32f1a
maharajafz03 <maharajafz1999@gmail.com>
2025-02-10 16:54:22 +0530

    [added]

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -1,39 +1,50 @@
-// struct Dash {
-//     name: String,
-//     age: u32
-// }
-
-// impl Dash {
-//      fn display(&self) {
-//         println!("{}", self.name);
-//         println!("{}", self.age);
-//      }
-
-//      fn modified(& mut self, name: &str, age: u32) {
-//         self.name = name.to_string();
-//         self.age = age;
-//      }
-// }
-
-// fn main() {
-//     let mut user = Dash{
-//         name: String::from("magaraja"),
-//         age: 25
-//     };
-//     user.display();
-//     user.modified("yamuna", 23);
-//     user.display();
-//     user.modified("sino", 23);
-//     user.display();
-// }
-
-fn main() {
-    let x = Some(5);
-
-    let a = (67);
-match x {
-    Some(val) => println!("Found: {}", val),
-    None => println!("No value found"),
+trait Racing {
+   fn mode(&self); 
 }
 
+struct Driver {
+    car_name: String,
+    fuel_type: String
+}
+
+struct Biker {
+    bike_name: String,
+    color: String
+}
+
+impl Racing for Driver {
+    
+    fn mode(&self) {
+        println!("F1_driver")
+    }
+
+    fn display(&self) {
+     println!("{} {}", self.car_name, self.fuel_type )
+    }
+}
+
+impl Racing for Biker {    
+    fn mode(&self) {
+        println!("MOTO_GP")
+    }
+}        
+
+fn render<T: Racing>(racer: T) {
+    racer.mode();
+}
+
+fn main() {
+
+    let user = Biker {
+        bike_name: String::from("ducati_panigale"),
+        color: String::from("red")
+    };
+
+    let user2 = Driver{
+        car_name: String::from("pagani"),
+        fuel_type: String::from("hybrid")
+    };
+
+    render(user);
+    render(user2);
 }
